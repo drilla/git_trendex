38e100b09193c46f7dd3e2cfb8b2952f6e3b65e1
drilla <drilla>
2020-10-13 16:27:06 +0300

    fix coveralls

--- mix.exs
+++ mix.exs
@@ -9,6 +9,8 @@ defmodule GitTrendex.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       aliases: aliases(),
       deps: deps()
     ]
